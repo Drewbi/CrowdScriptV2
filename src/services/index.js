aa2cde84de33575fc681b1ff3947e742efe0698f
Drew Alexander <rex.986@gmail.com>
2019-01-28 11:53:10 +0800

    Installed mongodb

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,7 @@
+const segment = require('./segment/segment.service.js');
+const submission = require('./submission/submission.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
+  app.configure(segment);
+  app.configure(submission);
 };
