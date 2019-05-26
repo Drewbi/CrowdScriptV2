3ac8a14f12528e65b12790914966ad6e3422880f
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-05-26 18:57:57 +0800

    Changing from feathers to express

--- test/services/segment.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'segment\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('segment');
-    expect(service).toBeTruthy();
-  });
-});
--- test/services/submission.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'submission\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('submission');
-    expect(service).toBeTruthy();
-  });
-});
