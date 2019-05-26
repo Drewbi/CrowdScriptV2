47b9ed63172278cce69af27e6af0385d717171aa
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-05-26 20:05:04 +0800

    Added landing page route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var landing = require('../controllers/landing');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', landing.get_landing);
 
 module.exports = router;
