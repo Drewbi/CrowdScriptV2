be63a34d69239b90055c9bba272918e636b57e24
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-06-28 03:52:46 +0800

    Added multiple routes and an api endpoint for testing

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const helmet = require('helmet')
 
-var indexRouter = require('./routes/index');
-var adminRouter = require('./routes/admin');
+const indexRouter = require('./routes/index');
+const adminRouter = require('./routes/admin');
+const apiRouter = require('./routes/api');
 
-var app = express();
-
-var { db } = require('./config/config.js')
-
-db.any('SELECT * FROM users')
-  .then(function (data) {
-    console.log('DATA:', data)
-  })
-  .catch(function (error) {
-    console.log('ERROR:', error)
-  })
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
+require('./routes/api')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
