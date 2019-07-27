b00ac2322337d76e43bc9ba2d5154dc9484906d7
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-07-28 00:06:54 +0800

    Added crypto functions

--- models/user.js
+++ models/user.js
@@ -1,25 +1,53 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+
 const { ObjectId } = mongoose.Schema.Types;
 
-module.exports = () => {
-  const userSchema = mongoose.Schema({
-    accessToken: String,
-    name: {
-      type: String,
-      required: true
-    },
-    credit: {
-      type: Boolean,
-      default: false
-    },
-    admin: {
-      type: Boolean,
-      default: false
-    },
-    submission: [{
-      type: ObjectId,
-      ref: 'Submission'
-    }]
-  });
-  return User;
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  credit: {
+    type: Boolean,
+    default: false
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  },
+  hash: {
+    type: String,
+    required: true,
+  },
+  salt: {
+    type: String,
+    required: true,
+  },
+  submission: [{
+    type: ObjectId,
+    ref: 'Submission'
+  }]
+});
+
+userSchema.pre('remove', function(next) {
+  this.model('Submission').deleteMany({ user: this._id }, next);
+  this.model('Session').deleteMany({ user: this._id }, next);
+});
+
+userSchema.methods.setPassword = function setPassword(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 };
+
+userSchema.methods.validatePassword = function validatePassword(password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+mongoose.model('User', userSchema);
