f34bce18a64f83678e772569b67b8044275968a2
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-07-29 18:49:01 +0800

    Added user controller

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+
+module.exports.addUser = (req, res) => {
+  const user = new User();
+
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.credit = req.body.credit;
+  user.setPassword(req.body.password);
+
+  user.save((err) => {
+    if (err) {
+      res.status(500).json({ message: err });
+    }
+  });
+};
