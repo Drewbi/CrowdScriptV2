9c933549ae1d677df38a324fb779945803e3bca4
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-08-12 13:58:37 +0800

    Added login functionallity and user getting

--- controllers/users.js
+++ controllers/users.js
@@ -4,37 +4,32 @@ const passport = require('passport');
 const User = mongoose.model('User');
 
 module.exports.addUser = (req, res) => {
-  console.log(req.body);
   const user = new User();
 
   user.name = req.body.name;
   user.email = req.body.email;
   user.credit = !!req.body.credit;
   user.setPassword(req.body.password);
-  console.log(user);
   user.save((err) => {
     if (err) {
-      console.log(err);
-      res.status(500).json({ message: err });
+      console.error('Error registering user');
+      res.redirect('/register');
     }
   });
 };
 
-module.exports.validateUser = (req, res) => {
-  console.log(req.body);
-  passport.authenticate('local', (err, user) => {
-    if (err) {
-      next(err);
-    }
-
-    if (!user) {
-      console.log('no user');
-      res.status(401).json({ message: err });
-      return;
-    }
-    req.logIn(user, (err2) => {
-      if (err2) { next(err); }
-      res.redirect('/admin');
+module.exports.validateUser = (req, res, next) => {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.redirect('/login'); }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      if (user.admin) { return res.redirect('/admin'); }
+      return res.redirect('/');
     });
   })(req, res, next);
 };
+
+module.exports.getAllUsers = () => {
+  return User.find({});
+}
