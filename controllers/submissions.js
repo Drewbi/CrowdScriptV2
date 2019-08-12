205bb472706f2aa0ecf5aa7002de4105f206b2cd
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-08-12 13:57:34 +0800

    Added submission controller

---
+++ controllers/submissions.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Submission = mongoose.model('Submission');
+
+module.exports.getSubmissions = (req, res) => {
+  return Submission.find({});
+};
+
+module.exports.createSubmissions = (req, res) => {
+  const submission = new Submission();
+
+  submission.text = req.body.text;
+  // submission.user = req
+  submission.save((err) => {
+    if (err) {
+      console.error('Error creating submission');
+    }
+  });
+};
