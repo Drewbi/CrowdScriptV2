a6f038e98151e3242d85287a91bf276ed07bf084
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-08-14 11:44:45 +0800

    Removed redirect from register

--- routes/register.js
+++ routes/register.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const { addUser } = require('../controllers/users')
+const express = require("express");
+const { addUser } = require("../controllers/users");
 const router = express.Router();
 
 /* GET register page. */
-router.get('/', (req, res) => {
-  res.render('register', {
-    title: 'Register',
+router.get("/", (req, res) => {
+  res.render("register", {
+    title: "Register"
   });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   console.log('"Registering" User');
   addUser(req, res);
-  res.redirect('/');
-})
+});
 
 module.exports = router;
