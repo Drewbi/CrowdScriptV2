36972aebb7d69dbeab29ae7eaeeaa266b560cc21
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-08-14 11:44:27 +0800

    Removed passport import

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const passport = require('passport');
-const { validateUser } = require('../controllers/users')
+const express = require("express");
+const { validateUser } = require("../controllers/users");
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  res.render('login', {
-    title: 'Login',
+router.get("/", (req, res) => {
+  res.render("login", {
+    title: "Login"
   });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   validateUser(req, res);
 });
 
