0a5d1e1dd900ba20d220c746c35ea798c347092b
Drewbi <22423862@student.uwa.edu.au>
2019-08-21 17:42:31 +0800

    Changed url encoded to extended mode

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set("view engine", "pug");
 app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
