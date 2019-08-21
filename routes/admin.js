219cd8ac188fd2e34183010493358cbc796ba4ad
Drewbi <22423862@student.uwa.edu.au>
2019-08-21 17:44:57 +0800

    Added multer support and made a work around for file naming

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const multer = require("multer");
-const upload = multer();
 const { getAllUsers } = require("../controllers/users");
 const { getSubmissions } = require("../controllers/submissions");
+const upload = require("../controllers/multer");
+const fs = require("fs");
 const router = express.Router();
 
 /* GET users listing. */
@@ -11,15 +11,29 @@ router.get("/", async (req, res) => {
     getAllUsers(),
     getSubmissions()
   ]);
-  console.log(users);
-
   res.render("admin", { title: "Admin", submissions, users });
 });
 
-router.post("/", upload.single("episodeAudio"), (req, res) => {
-  console.log(req.file);
-  console.log(req.body);
-  res.send(req.body);
+router.post("/", upload.single("episodeAudio"), (req, res, next) => {
+  console.log(req.body.episodeNum);
+  const file = req.file;
+  if (!file) {
+    const error = new Error("Please upload a file");
+    error.httpStatusCode = 400;
+    return next(error);
+  } else {
+    fs.rename(
+      `public/uploads/${file.originalname}`,
+      `public/uploads/${file.fieldname + req.body.episodeNum}.mp3`,
+      err => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  }
+  // uploadFile(req.file);
+  res.redirect("/admin");
 });
 
 module.exports = router;
