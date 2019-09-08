64101bab8a47496d1f8c5bb7211e6fa6285ca9de
Drewbi <22423862@student.uwa.edu.au>
2019-09-08 20:35:24 +0800

    Added segment creation

--- controllers/segment.js
+++ controllers/segment.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
 const Segment = mongoose.model("Segment");
-const Episode = mongoose.model("Episode");
 
-module.exports.getSegment = (epNum, segNum) => {
-  return Segment.find({  });
+module.exports.getSegment = (episodeID, segNum) => {
+  return Segment.find({ episode: episodeID, number: segNum });
+};
+
+module.exports.addSegment = (episode, text) => {
+  const segment = new Segment();
+  segment.episode = episode;
+  segment.text = text;
+  segment.save(err => {
+    if (err) {
+      console.error(`Error adding segment: ${err}`);
+    }
+  });
 };
