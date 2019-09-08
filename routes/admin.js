1b358f504c997a0bfaff75a4fb8a39bb49fbd8e1
Drewbi <22423862@student.uwa.edu.au>
2019-09-08 20:40:02 +0800

    Added srt parsing

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const { getSubmissions } = require("../controllers/submissions");
 const { addEpisode, getEpisodes } = require("../controllers/episode");
 const upload = require("../controllers/multer");
 const { ftpUpload } = require("../controllers/ftp");
+const parser = require('subtitles-parser');
 const fs = require("fs");
 const router = express.Router();
 
@@ -31,32 +32,32 @@ router.post(
     { name: "srtFile", maxCount: 1 }
   ]),
   (req, res, next) => {
-    const files = req.files;
-    if (Object.entries(files).length === 0) {
-      const error = new Error("Please upload a file");
-      error.httpStatusCode = 400;
-      return next(error);
+    if (!req.user) res.redirect('/login');
+    else {
+      const files = req.files;
+      if (Object.entries(files).length === 0) {
+        const error = new Error("Please upload a file");
+        error.httpStatusCode = 400;
+        return next(error);
+      }
+      const episodePath = "public/episodes/";
+      // SRT file processing
+      const [srtFile] = files.srtFile;
+      let data = fs.readFileSync(srtFile.path,'utf8');
+      data = data.replace(/(\d{2}:\d{2}:\d{2},\d{2})(\s)/g, '$10$2');
+      const srt = parser.fromSrt(data, true);
+      console.log(srt);
+      // Audio file processing
+      const [audioFile] = files.audioFile;
+      const audioFileName = `${audioFile.fieldname + req.body.episodeNum}.mp3`;
+      fs.rename(audioFile.path, episodePath + audioFileName, err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      // ftpUpload(audioFileName);
+      addEpisode(req, res);
     }
-    const episodePath = "public/episodes/";
-    // Audio file processing
-    const [audioFile] = files.audioFile;
-    const audioFileName = `${audioFile.fieldname + req.body.episodeNum}.mp3`;
-    fs.rename(audioFile.path, episodePath + audioFileName, err => {
-      if (err) {
-        console.log(err);
-      }
-    });
-    ftpUpload(audioFileName);
-    // SRT file processing
-    const [srtFile] = files.srtFile;
-    const srtFileName = `${srtFile.fieldname + req.body.episodeNum}.srt`;
-    fs.rename(srtFile.path, episodePath + srtFileName, err => {
-      if (err) {
-        console.log(err);
-      }
-    });
-    ftpUpload(srtFileName);
-    addEpisode(req, res);
   }
 );
 
