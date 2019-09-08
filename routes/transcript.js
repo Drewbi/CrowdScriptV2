ab41a344462851661c9ff64e5f7930d35c629854
Drewbi <22423862@student.uwa.edu.au>
2019-09-08 20:40:30 +0800

    Added dynamic episode choosing

--- routes/transcript.js
+++ routes/transcript.js
@@ -1,19 +1,25 @@
 const express = require("express");
+const { getLowestEpisode } = require("../controllers/episode");
+const { getSegment } = require("../controllers/segment");
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   if (!req.user) res.redirect("/about");
-  else res.render("transcript", {
-    title: "Transcripter",
-    episodeName: "Episode 230: Words about words",
-    placeholderText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  });
+  else {
+    const [episode] = await getLowestEpisode();
+    const segment = await getSegment(episode._id, 0);
+    console.log(episode);
+    res.render("transcript", {
+      title: "Transcripter",
+      episode: episode,
+      segment: segment
+    });
+  }
 });
 
 router.post("/", (req, res) => {
-  if (req.user) res.redirect("/");
+  // if (req.user) res.redirect("/");
 });
 
 module.exports = router;
