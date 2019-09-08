7727e6cc04035cf21afdaaab1f5451d2f9fe460d
Drewbi <22423862@student.uwa.edu.au>
2019-09-08 20:35:41 +0800

    Added basic episode queries

--- controllers/episode.js
+++ controllers/episode.js
@@ -3,14 +3,18 @@ const mongoose = require("mongoose");
 const Episode = mongoose.model("Episode");
 
 module.exports.getEpisodes = () => {
-  return Episode.find({})
+  return Episode.find();
+};
+
+module.exports.getLowestEpisode = () => {
+  return Episode.find().sort( { number: 1 } ).limit(1);
 };
 
 module.exports.addEpisode = (req, res) => {
   const episode = new Episode();
   episode.name = req.body.episodeName;
   episode.number = req.body.episodeNum;
-  episode.complted = false;
+  episode.completed = false;
   episode.save(err => {
     if (err) {
       console.error(`Error adding episode: ${err}`);
