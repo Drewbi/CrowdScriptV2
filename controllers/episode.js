e9557a158538976ec19329fb1b5e23914198ae3a
Drewbi <22423862@student.uwa.edu.au>
2019-09-13 02:36:30 +0800

    Changed episode creation to use promises

--- controllers/episode.js
+++ controllers/episode.js
@@ -10,17 +10,18 @@ module.exports.getLowestEpisode = () => {
   return Episode.find().sort( { number: 1 } ).limit(1);
 };
 
-module.exports.addEpisode = (req, res) => {
-  const episode = new Episode();
-  episode.name = req.body.episodeName;
-  episode.number = req.body.episodeNum;
-  episode.completed = false;
-  episode.save(err => {
-    if (err) {
-      console.error(`Error adding episode: ${err}`);
-      res.redirect("/admin");
-    } else {
-      res.redirect("/admin");
-    }
+module.exports.addEpisode = (req) => {
+  return new Promise((resolve, reject) => {
+    const episode = new Episode();
+    episode.name = req.body.episodeName;
+    episode.number = req.body.episodeNum;
+    episode.completed = false;
+    episode.save(err => {
+      if (err) {
+        reject(err);
+      }
+    });
+    console.log("Finished episode creation");
+    resolve(episode);
   });
 };
