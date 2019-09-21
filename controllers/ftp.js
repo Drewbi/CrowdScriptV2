3d04f2f391e6cb97a5462db64663d56a2b159df3
Drewbi <22423862@student.uwa.edu.au>
2019-09-21 20:02:50 +0800

    Restructered ftp uploading

--- controllers/ftp.js
+++ controllers/ftp.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports.uploadSegments = (episodeNum, numSegments) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     console.log("Uploading segments");
     const directory = `${process.env.PWD}/public/exports/${episodeNum}`;
     const ftpDirectory = `segments/${episodeNum}`;
@@ -13,35 +13,25 @@ module.exports.uploadSegments = (episodeNum, numSegments) => {
       password: process.env.FTP_PASS,
       host: process.env.FTP_HOST
     };
-    ftp.connect(config)
-      .then(() => {
-        return ftp.mkdir(ftpDirectory);
-      })
-      .then(() => {
-        let promises = [];
-        for (let i = 1; i <= numSegments; i++) {
-          const fileName = `${episodeNum}-${i}.mp3`;
-          const promise = ftp.put(
-            `${directory}/${fileName}`,
-            `${ftpDirectory}/${fileName}`
-          );
-          promises.push(promise);
-        }
-        return promises;
-      })
-      .then(() => {
-        return ftp.end();
-      })
-      .then(() => {
-        fs.readdir(directory, (err, files) => {
-          if (err) reject(err);
-          for (const file of files) {
-            fs.unlinkSync(path.join(directory, file));
-          }
-          fs.rmdirSync(directory);
-        });
-        console.log("Upload successful");
-        resolve();
-      });
+    await ftp.connect(config);
+    await ftp.mkdir(ftpDirectory);
+    const tasks = Array(numSegments).fill(null).map((_, index) => {
+      const fileName = `${episodeNum}-${index+1}.mp3`;
+      const promise = ftp.put( 
+        `${directory}/${fileName}`,
+        `${ftpDirectory}/${fileName}`);
+      return promise;
+    });
+    await Promise.all(tasks);
+    console.log("Segments uploaded");
+    ftp.end();
+    fs.readdir(directory, (err, files) => {
+      if (err) reject(err);
+      for (const file of files) {
+        fs.unlinkSync(path.join(directory, file));
+      }
+      fs.rmdirSync(directory);
+    });
+    resolve();
   });
 };
--- routes/transcript.js
+++ routes/transcript.js
@@ -1,21 +1,33 @@
 const express = require("express");
-const { getLowestEpisode } = require("../controllers/episode");
-const { getSegment } = require("../controllers/segment");
+const { getLowestEpisode, getEpisodeById } = require("../controllers/episode");
+const { getSegment, getSegmentBySlug } = require("../controllers/segment");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", async (req, res) => {
-  if (!req.user) res.redirect("/about");
+router.get("/:segmentId([A-F0-9]{10})", async (req, res) => {
+  if (!req.user) res.redirect("/login");
   else {
-    const [episode] = await getLowestEpisode();
-    let segment = null;
-    if(episode) segment = await getSegment(episode._id, 0);
+    const [segment] = await getSegmentBySlug(req.params.segmentId);
+    console.log(segment);
+    const [episode] = await getEpisodeById(segment.episode);
     console.log(episode);
     res.render("transcript", {
       title: "Transcripter",
       episode: episode,
       segment: segment
     });
+  }
+});
+/* GET users listing. */
+router.get("/", async (req, res) => {
+  if (!req.user) res.redirect("/about");
+  else {
+    const [episode] = await getLowestEpisode();
+    console.log(episode);
+    console.log("Epsiode found");
+    const [segment] = await getSegment(episode._id, 0);
+    console.log(segment);
+    if(segment) return res.redirect("/" + segment.slug);
+    else res.render("404");
   }
 });
 
