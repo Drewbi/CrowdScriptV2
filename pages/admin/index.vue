67f401bf369351ebc7bb36a07078626fece35a5b
Drew Alexander <drewalexander986@gmail.com>
2020-06-24 23:13:13 +0800

    Fix auto login (#12)
    * Update auth logic
    
    * Update Admin page to async data
    
    * Update transcripter to async data

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -29,41 +29,41 @@ export default {
     loading: true,
     dialog: false
   }),
-  async mounted() {
-    try {
-      const userPromise = this.$axios.get('/api/user')
-      const episodePromise = this.$axios.get('/api/episode')
-      const segmentPromise = this.$axios.get('/api/segment')
-      const submissionPromise = this.$axios.get('/api/submission')
-      const [userData, episodeData, segmentData, submissionData] = await Promise.all([userPromise, episodePromise, segmentPromise, submissionPromise])
-      const { users } = userData.data
-      const { episodes } = episodeData.data
-      const { segments } = segmentData.data
-      const { submissions } = submissionData.data
-      segments.forEach((segment) => {
-        const segmentSubmissions = submissions.filter(submission => submission.segment === segment._id)
-        segment.submissions = segmentSubmissions
-      })
-      episodes.forEach((episode) => {
-        const episodeSegments = segments.filter(segment => segment.episode === episode._id)
-        const episodeSubmissions = []
-        episodeSegments.forEach((segment) => {
-          episodeSubmissions.push(...segment.submissions)
-        })
-        episode.segments = episodeSegments
-        episode.submissions = episodeSubmissions
-      })
-      users.forEach((user) => {
-        const userSubmissions = submissions.filter(submission => submission.user === user._id)
-        user.submissions = userSubmissions
-      })
-      this.users = users
-      this.episodes = episodes
-      this.loading = false
-    } catch (err) {
-      this.setError(err.response.data.message)
-      this.$router.push('/about')
+  async asyncData({ app, redirect, error }) {
+    const userPromise = app.$axios.get('/api/user')
+    const episodePromise = app.$axios.get('/api/episode')
+    const segmentPromise = app.$axios.get('/api/segment')
+    const submissionPromise = app.$axios.get('/api/submission')
+    const [
+      { data: { users } },
+      { data: { episodes } },
+      { data: { segments } },
+      { data: { submissions } }
+    ] = await Promise.all([userPromise, episodePromise, segmentPromise, submissionPromise])
+    if (!users || !episodes || !segments || !submissions) {
+      return error({ statusCode: 400, message: 'Could not get data' })
     }
+    segments.forEach((segment) => {
+      const segmentSubmissions = submissions.filter(submission => submission.segment === segment._id)
+      segment.submissions = segmentSubmissions
+    })
+    episodes.forEach((episode) => {
+      const episodeSegments = segments.filter(segment => segment.episode === episode._id)
+      const episodeSubmissions = []
+      episodeSegments.forEach((segment) => {
+        episodeSubmissions.push(...segment.submissions)
+      })
+      episode.segments = episodeSegments
+      episode.submissions = episodeSubmissions
+    })
+    users.forEach((user) => {
+      const userSubmissions = submissions.filter(submission => submission.user === user._id)
+      user.submissions = userSubmissions
+    })
+    return { users, episodes, segments, submissions }
+  },
+  mounted() {
+    this.loading = false
   },
   methods: {
     ...mapMutations(['setError']),
